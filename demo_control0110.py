509b44452adb34eaca0ae669506965f33af8e1db
SenseLand <123456789@qq.com>
2020-09-11 15:36:17 +0800

    测试git

--- demo_control0110.py
+++ demo_control0110.py
@@ -9,7 +9,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import pyqtSignal
 
 from signalslot import slot                   #信号槽文件 
-from transfer import trans                    #通信文件
+from transfer import trans                    #通信文件 
 from Ui_controlUI0319 import Ui_Control  
 from Ui_pw1dialog0110 import Ui_Dialog       
 from Ui_seaddialog import Ui_seedDialog
